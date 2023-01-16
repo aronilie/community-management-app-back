916455957a7d3fb0662e179c383223d4c7d21699
Aron Ilie <ailie@jnc.es>
2023-01-16 17:10:48 +0100

    Added users controller

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,15 @@ import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 import { generalError, notFoundError } from "../errors/generalError";
+import usersRouter from "./routes/usersRouter";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 app.use(notFoundError);
 app.use(generalError);
